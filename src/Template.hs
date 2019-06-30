8f054194e0b209ac19a98f5869b9da87c2e60603
Paul Khuzhin <poul1298@gmail.com>
2019-06-30 20:44:07 +0300

    temp

--- src/Template.hs
+++ src/Template.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Template where
+-- import qualified Data.ByteString.Char8 as B
+import qualified Data.Text as T
 
-header1 = unlines ["    ПРОТОКОЛ"
-                  ,"патологоанатомического вскрытия №"]
+type RTF = String
 
-tes = "Ты должен поменяться"                  
+-- Протокол
+field1 :: [String] -> RTF
+field1 ss = ss !! 0 ++ "\n"
+field2 = ""
+field3 = ""
+field4 = ""
+field5 = ""
+field6 = ""
+field7 = ""
+field8 = ""
+field9 = ""
+field10 = ""
+field11 = ""
+field12 = ""
+field13 = ""
+field14 = ""
+field15 = ""
+field16 = ""
+field17 = ""
+field18 = ""
--- src/Writer.hs
+++ src/Writer.hs
@@ -1,11 +1,49 @@
 module Writer where
-import Graphics.PDF
+import Data.List(intercalate)
 
-main :: IO ()
-main = do
-  let pdfFileName= "test1.pdf"
-  let documentInfo = standardDocInfo
-  let defaultPageSize = PDFRect 0 0 200 300
+pathFile = "test.rtf"
 
-  runPdf pdfFileName documentInfo defaultPageSize $ do
-    addPage Nothing
+data Format = Bold | Roman
+
+instance Show Format where
+  show Bold  = "\\b "
+  show Roman = ""
+
+data Quadding = QLeft | QCenter | QRight | QJustify
+
+instance Show Quadding where
+  show QLeft    = "\\ql "
+  show QCenter  = "\\qc "
+  show QRight   = "\\qr "
+  show QJustify = "\\qj "
+
+data RTFString = RTFString { format :: Format
+                           , content :: String
+                           }
+
+instance Show RTFString where
+  show (RTFString f c) = show f ++ show c
+  showList cs = (++) $ intercalate "\n" (map show cs)
+
+data Paragraph = Paragraph { alignment :: Quadding
+                           , contents :: [RTFString]
+                           }
+
+instance Show Paragraph where
+  show (Paragraph a cs) = "{\\pard" ++ show a ++ "\n" ++ showList cs "" ++ "\n\\par}\n"
+
+appendRTFStringOrPara :: (Show a) => a -> IO ()
+appendRTFStringOrPara s = appendFile pathFile (show s)
+
+startRTF = "{\\rtf1\\ansi\\deff0\\fs22 {\\fonttbl {\\f0 Times New Roman;}}\n"
+
+test = Paragraph QJustify [RTFString Bold "qwdqwdqwd"]
+
+writeRTF :: IO ()
+writeRTF = do
+  file <- readFile "text.txt"
+  let templateFields = lines file
+  let f = [RTFString Bold x | x <- templateFields]
+  writeFile pathFile startRTF
+  mapM appendRTFStringOrPara f
+  appendFile pathFile "}"
