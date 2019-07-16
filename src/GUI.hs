28f5b67f525b52e7b01703b6815d26e43262bc27
Paul Khuzhin <poul1298@gmail.com>
2019-07-16 13:08:53 +0300

    added ~cb

--- src/GUI.hs
+++ src/GUI.hs
@@ -1,9 +1,12 @@
 -- {-# LANGUAGE TemplateHaskell #-}
 module GUI where
+import           Control.Concurrent
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.IORef
 import           Data.Maybe                  (fromMaybe)
+import qualified Data.Text                   as T
+import qualified Data.Vector.Mutable         as M
 import           Graphics.UI.Gtk             hiding (Action, backspace)
 import           Graphics.UI.Gtk.Layout.Grid
 import           System.IO
@@ -13,12 +16,14 @@ import           Writer
 
 startGUI :: Handle -> IO ()
 startGUI out = do
+  a <- M.new 7
   void initGUI
   window <- windowNew
   set window [ windowTitle         := "ПаТаН"
              -- , windowDeletable     := False
              , windowDefaultWidth  := 500
              , windowDefaultHeight := 600 ]
+  -- toArray
   fields <- sequence $ replicate 7 entryNew
   -- attrs = [ ent]
   sequence_ [set x [entryPlaceholderText := Just temp] | (x, temp) <- zip fields text] -- setup fields
@@ -26,13 +31,22 @@ startGUI out = do
   -- gridSetRowHomogeneous grid1 True -- rows same height
   gridSetColumnHomogeneous grid1 True
   --
-  sequence_ [gridAttach grid1 field 0 i 4 1 | (field, i) <- zip fields [0..6]]
+  -- sequence_ [gridAttach grid1 field 0 i 4 1 | (field, i) <- zip fields [0..6]]
 
   cal <- calendarNew -- calendar
-  gridAttach grid1 cal 0 7 4 1
+  -- gridAttach grid1 cal 0 7 4 1
 
   tb <- checkButtonNewWithLabel "Готово"
-  gridAttach grid1 tb 3 8 1 1
+  -- gridAttach grid1 tb 3 8 1 1
+
+  -- ls <- listStoreNew ["asd", "dasd"]
+  cb <- comboBoxNewText
+  set cb [comboBoxTitle := "4. Пол:"]
+  comboBoxPrependText cb $ T.pack "Мужской"
+  comboBoxPrependText cb $ T.pack "Женский"
+  -- comboBoxSetTitle cb "4. Пол:"
+  comboBoxGetTitle cb >>= putStrLn
+  gridAttach grid1 cb 0 9 1 1
 
   containerAdd window grid1
 
@@ -41,7 +55,9 @@ startGUI out = do
   widgetShowAll window
 
   -- signals section
-  tb `on` toggled $ liftIO mainQuit
+  tb `on` toggled $ do
+    liftIO mainQuit
+    -- threadDelay 1000000 --sleep for a million microseconds, or one second
 
   let activ f = f `on` entryActivated $ do
               q <- entryGetPlaceholderText f
