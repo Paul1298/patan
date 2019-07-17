ab0a8e1df2bd456130385cecd899ea14ac888d74
Paul Khuzhin <poul1298@gmail.com>
2019-07-17 09:58:27 +0300

    win go win!

--- src/GUI.hs
+++ src/GUI.hs
@@ -3,12 +3,13 @@ import           Control.Concurrent
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.IORef
-import           Data.List                   (elemIndex)
+import           Data.List                   (elemIndex, maximumBy)
 import           Data.Maybe                  (fromMaybe)
 import qualified Data.Text                   as T
 import           Data.Vector                 (freeze, toList)
 import qualified Data.Vector.Mutable         as M
-import           Graphics.UI.Gtk             hiding (Action, backspace)
+import           Graphics.UI.Gtk
+--hiding (Action, backspace)
 import           Graphics.UI.Gtk.Layout.Grid
 import           System.IO
 import           System.Process
@@ -29,24 +30,34 @@ startGUI = do
   out <- initRTF
   void initGUI
   window <- windowNew
-  set window [ windowTitle         := "ПаТаН" ]
-             -- , windowDeletable     := False
-             -- , windowDefaultWidth  := 500
+  set window [ windowTitle         := "ПаТаН"
+             , windowDefaultWidth  := 1000 ]
              -- , windowDefaultHeight := 600 ]
+  -- putStrLn $ show . length $ maximumBy (\a b -> if length a > length b then GT else LT) text1
   -- toArray
   let n = length text1
 
+  temps <- sequence $ replicate n entryNew
   fields <- sequence $ replicate n entryNew
   -- attrs = [ ent]
-  sequence_ [set x [entryPlaceholderText := Just temp] | (x, temp) <- zip fields text1] -- setup fields
+  sequence_ [set x [ entryEditable := False, entryText := temp] | (x, temp) <- zip temps text1] -- setup fields
   grid1 <- gridNew
   -- gridSetRowHomogeneous grid1 True -- rows same height
   gridSetColumnHomogeneous grid1 True
+  cb <- comboBoxNewText
+  set cb [comboBoxTitle := "Пол"]
+  -- comboBoxSetTitle cb "asd"
+  comboBoxPrependText cb $ T.pack "Мужской"
+  comboBoxPrependText cb $ T.pack "Женский"
+  -- comboBoxSetActive cb 0
+  -- comboBoxGetTitle cb >>= putStrLn
+  gridAttach grid1 cb 3 3 1 1
   --
-  sequence_ [gridAttach grid1 field 0 i 4 1 | (field, i) <- zip fields [0..n - 1]]
+  sequence_ [gridAttach grid1 temp 0 i 3 1 | (temp, i) <- zip temps [0..n - 1]]
+  sequence_ [gridAttach grid1 field 3 i 1 1 | (field, i) <- zip fields [0..n - 1]]
 
-  cal <- calendarNew -- calendar
-  gridAttach grid1 cal 0 n 4 1
+  -- cal <- calendarNew -- calendar
+  -- gridAttach grid1 cal 0 n 1 1
 
   tb <- checkButtonNewWithLabel "Готово"
   gridAttach grid1 tb 3 (n + 1) 1 1
@@ -54,12 +65,6 @@ startGUI = do
   -- exitb <- checkButtonNewWithLabel "Выход"
   -- gridAttach grid1 tb 3 9 1 1
 
-  -- cb <- new comboBoxNewText [comboBoxTitle := "4. Пол:"]
-  -- -- set cb [comboBoxTitle := "4. Пол:"]
-  -- comboBoxPrependText cb $ T.pack "Мужской"
-  -- comboBoxPrependText cb $ T.pack "Женский"
-  -- comboBoxGetTitle cb >>= putStrLn
-  -- gridAttach grid1 cb 0 9 1 1
 
   containerAdd window grid1
 
@@ -78,12 +83,14 @@ startGUI = do
   -- Готово
   tb `on` toggled $ do
     buttonSetLabel tb "Не нажимать"
-    r <- createProcess (proc "loffice" [pathFile]) -- linux
-    -- r <- createProcess (proc "start" [pathFile]) -- win
     tmp <- freeze mv
     writeText1 out (toList tmp)
     endRTF out
     hClose out
+    r <- createProcess (proc "loffice" [pathFile]) --linux
+    -- pid <- runCommand ("start " ++ pathFile) --win
+    -- r <- createProcess (proc "start" [pathFile]) { create_new_console = True } -- win
+    return ()
 
   window `on` deleteEvent $ liftIO mainQuit >> return False
 
