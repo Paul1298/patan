16f49c8924acd906677f3791050491fc530f1b0a
Paul Khuzhin <poul1298@gmail.com>
2019-07-22 12:39:49 +0300

    test entry-completion

--- src/GUI.hs
+++ src/GUI.hs
@@ -2,10 +2,8 @@ module GUI where
 -- import           Control.Concurrent
 -- import           Control.Monad
 import           Control.Monad.IO.Class
-import qualified Data.Text              as T
-import qualified Data.Vector            as V
---(freeze, fromList, toList)
-import qualified Data.Vector.Mutable    as M
+import           Data.List              (isPrefixOf)
+import           Data.Text              (pack)
 import           Graphics.UI.Gtk
 --hiding (Action, backspace)
 import           System.IO
@@ -23,39 +21,62 @@ initRTF = do
   startRTF out
   return out
 
-signals :: Int -> V.Vector Entry -> CheckButton -> Window -> IO (ConnectId Window)
-signals n fields checkb window = do
+getEntry :: ComboBox -> IO Entry
+getEntry c = do
+  Just w <- binGetChild c
+  return (castToEntry w)
+--
+
+s = def1 !! 9
+
+textColumn = makeColumnIdString 0
+
+-- test c = do
+--    f <- getEntry c
+--    _ <- f `on` editableChanged $ do
+--      t <- entryGetText f
+--      let tmp = filter (isPrefixOf t) s
+--      -- putStrLn tmp
+--      if (not $ null tmp)
+--        then
+--          do
+--          customStoreSetColumn store textColumn (\x -> pack x)
+--
+--        -- combos <- sequence [comboBoxNewWithModelAndEntry store | store <- stores ]
+--          comboBoxSetModel c (Just store)
+--          -- comboBoxPopup c
+--        else
+--          return ()
+--    return ()
+
+
+signals :: [ComboBox] -> CheckButton -> Window -> IO (ConnectId Window)
+signals combos checkb window = do
+  fields <- mapM getEntry combos
   -- signals section
-  replicate
-  rep
-  mv <- M.replicate n ""
 
-  -- Enrties
-  let activ f = f `on` entryActivated $ do --use this!
-                a <- entryGetText f :: IO String
-                let (Just i) = f `V.elemIndex` fields
-                M.write mv i a
-  mapM_ activ fields
+  -- Autocomplete
+  let auto c = c `on` changed $ do
+        -- test c
+        undefined
+    -- f <- containerGetChildren c
+    -- f `on` editableChanged $ do
+    -- -- entryGetText f >>= putStrLn
+        -- undefined
 
-  -- -- test
-  -- (fields V.! 0) `on` editableChanged $ do
-  --   putStrLn "Ай"
-    -- return False
-    -- st <- widgetGetState (fields V.! 0)
-    -- foo st
+  mapM_ auto combos
 
   -- Готово
   _ <- checkb `on` toggled $ do
     out <- initRTF
-    -- buttonSetLabel tb "Не нажимать"
-    tmp <- V.freeze mv
-    writeText1 out (V.toList tmp)
+    mapM entryGetText fields >>= writeText1 out
     endRTF out
     hClose out
     _ <- createProcess (proc "loffice" [pathFile]) --linux
     -- _ <- runCommand ("start " ++ pathFile) --win
     return ()
 
+  -- Закрытие окна
   window `on` deleteEvent $ liftIO mainQuit >> return False
 
 startGUI :: IO ()
@@ -75,42 +96,31 @@ startGUI = do
   sequence_ [gridAttach grid1 l 0 i 3 1 | (l, i) <- zip labels [0..n - 1]] -- attach them
 
   -- entries with comboBox
-  -- fields <- sequence . V.fromList $ replicate n entryNew
-
-  let textColumn = makeColumnIdString 0
-
   stores <- sequence [listStoreNew def | def <- def1]
-  sequence_ [customStoreSetColumn store textColumn (\x -> T.pack x) | store <- stores ] -- set the extraction function
+  sequence_ [customStoreSetColumn store textColumn (\x -> pack x) | store <- stores ] -- set the extraction function
 
   combos <- sequence [comboBoxNewWithModelAndEntry store | store <- stores ]
   sequence_ [comboBoxSetEntryTextColumn combo textColumn | combo <- combos ] -- set which column should be used
   sequence_ [gridAttach grid1 field 3 i 1 1 | (field, i) <- zip combos [0..n - 1]]
 
-  ens <- mapM containerGetChildren combos
-  let fields = V.fromList $ (map (castToEntry . head) ens)
-
   checkb <- checkButtonNewWithLabel "Готово"
   gridAttach grid1 checkb 3 (n + 1) 1 1
-
-  -- ls <- listStoreNew ["ыв", "авы", "ыва"]
-  -- cb <- comboBoxNewWithModel ls
-
-  -- comboBoxPrependText cb $ T.pack "Мужской"
-  -- comboBoxPrependText cb $ T.pack "Женский"
-  -- comboBoxSetActive cb 0
-  -- comboBoxGetTitle cb >>= putStrLn
-  -- gridAttach grid1 cb 3 3 1 1
-  --
-  -- cls <- comboBoxSetModelText cb >>= listStoreToList
 
   -- cal <- calendarNew -- calendar
   -- gridAttach grid1 cal 0 n 1 1
 
+  q <- entryCompletionNew
+  store <- listStoreNew s
+  customStoreSetColumn store textColumn (\x -> pack x)
+  entryCompletionSetModel q $ Just store
+  entryCompletionSetTextColumn q textColumn
+  e <- entryNew
+  entrySetCompletion e q
+  gridAttach grid1 e 0 (n + 1) 1 1
 
-  -- combo `on` entryActivated $ putStrLn "Ай"
   containerAdd window grid1
   widgetShowAll window
 
-  _ <- signals n fields checkb window
+  _ <- signals combos checkb window
   -- start!
   mainGUI
