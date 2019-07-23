f5a31097fbe689efbdd5b6608382cf3e5da19c83
Paul Khuzhin <poul1298@gmail.com>
2019-07-23 10:03:14 +0300

    2 test entry-completion

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main where
+import           Graphics.UI.Gtk
+
 import           GUI
 main :: IO ()
 main = do
+  _ <- initGUI
   startGUI
+  mainGUI
--- src/GUI.hs
+++ src/GUI.hs
@@ -3,7 +3,7 @@ module GUI where
 -- import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.List              (isPrefixOf)
-import           Data.Text              (pack)
+import           Data.Text              (pack, unpack)
 import           Graphics.UI.Gtk
 --hiding (Action, backspace)
 import           System.IO
@@ -25,51 +25,17 @@ getEntry :: ComboBox -> IO Entry
 getEntry c = do
   Just w <- binGetChild c
   return (castToEntry w)
---
-
-s = def1 !! 9
 
 textColumn = makeColumnIdString 0
 
--- test c = do
---    f <- getEntry c
---    _ <- f `on` editableChanged $ do
---      t <- entryGetText f
---      let tmp = filter (isPrefixOf t) s
---      -- putStrLn tmp
---      if (not $ null tmp)
---        then
---          do
---          customStoreSetColumn store textColumn (\x -> pack x)
---
---        -- combos <- sequence [comboBoxNewWithModelAndEntry store | store <- stores ]
---          comboBoxSetModel c (Just store)
---          -- comboBoxPopup c
---        else
---          return ()
---    return ()
-
-
-signals :: [ComboBox] -> CheckButton -> Window -> IO (ConnectId Window)
-signals combos checkb window = do
-  fields <- mapM getEntry combos
+signals :: [Entry] -> CheckButton -> Window -> IO (ConnectId Window)
+signals entries checkb window = do
   -- signals section
-
-  -- Autocomplete
-  let auto c = c `on` changed $ do
-        -- test c
-        undefined
-    -- f <- containerGetChildren c
-    -- f `on` editableChanged $ do
-    -- -- entryGetText f >>= putStrLn
-        -- undefined
-
-  mapM_ auto combos
 
   -- Готово
   _ <- checkb `on` toggled $ do
     out <- initRTF
-    mapM entryGetText fields >>= writeText1 out
+    mapM entryGetText entries >>= writeText1 out
     endRTF out
     hClose out
     _ <- createProcess (proc "loffice" [pathFile]) --linux
@@ -81,7 +47,6 @@ signals combos checkb window = do
 
 startGUI :: IO ()
 startGUI = do
-  _ <- initGUI
   window <- windowNew
   set window [ windowTitle          := "ПаТаН" ]
   grid1 <- gridNew
@@ -93,15 +58,28 @@ startGUI = do
   -- labels
   labels <- sequence [labelNew $ Just t | t <- text1] -- init labels
   sequence_ [miscSetAlignment l 0 0 | l <- labels] -- left alignment labels
-  sequence_ [gridAttach grid1 l 0 i 3 1 | (l, i) <- zip labels [0..n - 1]] -- attach them
+  sequence_ [gridAttach grid1 l 0 i 3 1 | (l, i) <- zip labels [0..n - 1]] --attach them
 
   -- entries with comboBox
   stores <- sequence [listStoreNew def | def <- def1]
   sequence_ [customStoreSetColumn store textColumn (\x -> pack x) | store <- stores ] -- set the extraction function
 
+  -- combos
   combos <- sequence [comboBoxNewWithModelAndEntry store | store <- stores ]
   sequence_ [comboBoxSetEntryTextColumn combo textColumn | combo <- combos ] -- set which column should be used
   sequence_ [gridAttach grid1 field 3 i 1 1 | (field, i) <- zip combos [0..n - 1]]
+
+  -- entries
+  entries <- mapM getEntry combos
+
+  -- entry-completion
+  ecompls <- sequence $ replicate n entryCompletionNew
+  sequence_ [set ec [ entryCompletionModel            := Just st
+                    , entryCompletionMinimumKeyLength := 0
+                    , entryCompletionTextColumn       := textColumn] | (ec, st) <- zip ecompls stores]
+
+  -- sequence_ [entryCompletionSetMinimumKeyLength ec 0 | ec <- ecompls]
+  sequence_ [entrySetCompletion e ec | (e, ec) <- zip entries ecompls]
 
   checkb <- checkButtonNewWithLabel "Готово"
   gridAttach grid1 checkb 3 (n + 1) 1 1
@@ -109,18 +87,9 @@ startGUI = do
   -- cal <- calendarNew -- calendar
   -- gridAttach grid1 cal 0 n 1 1
 
-  q <- entryCompletionNew
-  store <- listStoreNew s
-  customStoreSetColumn store textColumn (\x -> pack x)
-  entryCompletionSetModel q $ Just store
-  entryCompletionSetTextColumn q textColumn
-  e <- entryNew
-  entrySetCompletion e q
-  gridAttach grid1 e 0 (n + 1) 1 1
-
   containerAdd window grid1
+
   widgetShowAll window
 
-  _ <- signals combos checkb window
-  -- start!
-  mainGUI
+  _ <- signals entries checkb window
+  return ()
