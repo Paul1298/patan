6d4285e59fc541c4ccb869adc7040be0aee85442
Paul Khuzhin <poul1298@gmail.com>
2019-08-05 17:03:12 +0300

    test 2 sect om wind

--- src/GUI.hs
+++ src/GUI.hs
@@ -1,31 +1,36 @@
 module GUI where
 
 import           Control.Monad.IO.Class
-import           Data.Text              (Text)
 import           Graphics.UI.Gtk        hiding (AlignCenter)
 -- import           Graphics.UI.Gtk.General.Enums (Align (AlignCenter))
-import           System.IO
-import           System.Process
 
 import           CommonGUI
 import           DefCombo
 import           Labels
 import           Signals
-import           Writer
 
 startGUI :: IO ()
 startGUI = do
-  window <- windowNew --{ windowTitle = "ПаТаН" }
-  set window [
-               windowDefaultWidth  := 500
-             , windowDefaultHeight := 500
+  window <- windowNew
+  set window [ windowTitle         := "ПаТаН"
+             , windowDefaultWidth  := 800
+             , windowDefaultHeight := 800
              ]
   let n1 = length labels1
   (grid1, entries1) <- initGrid n1 labels1 initDef1
+  sign1sect grid1 entries1
 
   butt1_2 <- buttonNewWithLabel "Клинические данные"
-  gridAttach grid1 butt1_2 2 n1 1 1
-  sign1sect grid1 entries1 butt1_2
+  butt <- buttonNew
+  gridAttach grid1 butt1_2 1 n1 1 1
+
+  en <- entryNew
+  c <- comboBoxNew
+  -- let a = [(castToWidget en), (castToWidget c)]
+  box <- hBoxNew False 0
+  boxPackStart box en PackGrow 0
+  boxPackEnd box butt PackNatural 0
+  gridAttach grid1 box 0 n1 1 1
 
 
   let n2 = length labels2
@@ -40,17 +45,19 @@ startGUI = do
 
   butt2_1 <- buttonNewWithLabel "Макроскопическое исследование"
   butt2_3 <- buttonNewWithLabel "Готово"
+  gridAttach grid2 butt2_1 0 n2 1 1
+  gridAttach grid2 butt2_3 1 n2 1 1
 
-  -- containerGetChildren (exps !! 0) >>=  widgetGetName . last >>= putStrLn
-  -- sp <- spinButtonNewWithRange 0 90 0.1
-  -- gridAttach grid2 sp 0 n 1 1
+  containerGetChildren (exps !! 0) >>=  widgetGetName . last >>= putStrLn
+  sp <- spinButtonNewWithRange 0 90 0.1
+  gridAttach grid2 sp 0 (n2 + 1) 1 1
 
   sw <- scrolledWindowNew Nothing Nothing
   containerAdd sw grid1
   containerAdd window sw
-  widgetShowAll window
-
   signSectChange sw [grid1, grid2] [butt1_2, butt2_1, butt2_3] [entries1]
+
+  widgetShowAll window
 
   -- windowMaximize window
   _ <- window `on` deleteEvent $ liftIO mainQuit >> return False -- Закрытие окна
