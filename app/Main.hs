7303bf898c6323f4a63a21e54c2e2a6c332d3495
Paul Khuzhin <poul1298@gmail.com>
2019-10-25 21:33:52 +0300

    Revert "let test GS on Wind"
    This reverts commit 801a181f9895ad1195105dff47bc3a2d9039bac3.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
+<<<<<<< HEAD
+=======
+import           Control.Monad   (void)
+import           Graphics.UI.Gtk
+
+>>>>>>> parent of 801a181... let test GS on Wind
 import           GUI.GUI
 
 main :: IO ()
 main = do
+<<<<<<< HEAD
   startGUI
+=======
+  void initGUI
+  startGUI
+  mainGUI
+>>>>>>> parent of 801a181... let test GS on Wind
